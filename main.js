72b14673096cf46b37b97b28447a75cc0dd202ca
Sergey Veremiychuk <veremey4uk@gmail.com>
2024-02-12 20:35:37 +0100

    add data

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ const app = Vue.createApp({
 	data() {
 		return {
 			title: "Comedy event",
+			tickets: 0,
+			img: "https://picsum.photos/id/42/200/300",
+			imgAlt: "cafe",
+			img2: "https://picsum.photos/id/15/200/300",
+			imgAlt: "cafe",
+			img2: "berge",
 		}
 	},
 })
