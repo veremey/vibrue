cb2c2ce017f817cb0e99230f8f7ced24e404fcdc
Sergey Veremiychuk <veremey4uk@gmail.com>
2024-02-13 09:59:57 +0100

    v-ifi directive

--- main.js
+++ main.js
@@ -2,12 +2,11 @@ const app = Vue.createApp({
 	data() {
 		return {
 			title: "Comedy event",
-			tickets: 0,
+			tickets: 2,
 			img: "https://picsum.photos/id/42/200/300",
 			imgAlt: "cafe",
 			img2: "https://picsum.photos/id/15/200/300",
-			imgAlt: "cafe",
-			img2: "berge",
+			img2Alt: "berge",
 		}
 	},
 })
