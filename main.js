c4c259df50c2dc07539d00d128bebc06773e8cf5
Sergey Veremiychuk <veremey4uk@gmail.com>
2024-02-15 11:04:34 +0100

    v-on dirrective and vue method

--- main.js
+++ main.js
@@ -2,12 +2,19 @@ const app = Vue.createApp({
 	data() {
 		return {
 			title: "Comedy event",
-			tickets: 2,
+			tickets: 10,
+			cart: 0,
 			img: "https://picsum.photos/id/42/200/300",
 			imgAlt: "cafe",
 			img2: "https://picsum.photos/id/15/200/300",
 			img2Alt: "berge",
 			details: ["Funny comics", "Mime artistry", "Satire", "Sketches"],
 		}
+	},
+	methods: {
+		addCart(number = 1) {
+			this.cart += number
+			this.tickets -= number
+		},
 	},
 })
