b1d3fba341418e1b8e55fdcb9dbd51a81d2aef0a
Sergey Veremiychuk <veremey4uk@gmail.com>
2024-02-17 12:51:43 +0100

    computed props

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const app = Vue.createApp({
 			title: "Comedy event",
 			tickets: 10,
 			cart: 0,
-			image: "https://picsum.photos/id/48/200/300",
-			imageAlt: "fer",
+			image: "https://picsum.photos/id/15/200/300",
+			imageAlt: "Bronze",
 			img: "https://picsum.photos/id/42/200/300",
 			imgAlt: "cafe",
 			img2: "https://picsum.photos/id/15/200/300",
@@ -44,6 +44,11 @@ const app = Vue.createApp({
 		updateImage(id) {
 			this.image = this.ticketTypes.filter((type) => type.id === id)[0].image
 			this.imageAlt = this.ticketTypes.filter((type) => type.id === id)[0].name
+		},
+	},
+	computed: {
+		soldOut() {
+			return this.tickets === 0
 		},
 	},
 })
