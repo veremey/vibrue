1f98f151a3b172057bd8365163e86d3123ae6329
Sergey Veremiychuk <veremey4uk@gmail.com>
2024-02-23 10:17:14 +0100

    etv

--- components/TicketDetails.js
+++ components/TicketDetails.js
@@ -10,14 +10,22 @@ app.component("ticket-details", {
       <p>{{description}}</p>
       <select v-model="ticketsToBuy">
         <option value=0>0</option>
-        <option v-for="number in tickets" :value="number">{{number}}</option>
+        <option v-for="number in ticketsLeft" :value="number">{{number}}</option>
       </select>
+      <button v-if="ticketsToBuy > 0" @click="addTickets(ticketsToBuy)"> Adds tickets </button>
     </div>
   `,
 
 	data() {
 		return {
 			ticketsToBuy: 0,
+			ticketsLeft: this.tickets,
 		}
+	},
+	methods: {
+		addTickets(num) {
+			this.ticketsLeft -= num
+			this.ticketsToBuy = 0
+		},
 	},
 })
